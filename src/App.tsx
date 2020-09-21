411a496a1269ebb3a8938f7fd4aed834ec5f2757
Henk Klijn Hesselink <klijnh@oclc.org>
2020-09-21 12:49:54 +0200

    CHORE first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const APIURL = "https://teletekst-data.nos.nl/json?p=";
+
+
+export interface TeleTekstObject {
+  prevPage: string;
+  nextPage: string;
+  prevSubPage: string;
+  nextSubPage: string;
+  fastTextLinks: {
+    title: string;
+    page: string;
+  }[];
+  content: string;
+}
+
+export const getTeletekstPage = async ( pageNumber:number)  => {
+  const res = await fetch(`${APIURL}${pageNumber}`);
+  return res.json();
+};
+
+
+
+const App: React.FC = () => {
+
+  const [pageData, setPageData] = React.useState<TeleTekstObject>({
+    prevPage: "string",
+    nextPage: "string",
+    prevSubPage: "string",
+    nextSubPage: "string",
+    fastTextLinks: [],
+    content: "loading ... "
+  });
+  React.useEffect(
+    () => {
+  
+      getTeletekstPage(100).then( (pd) => {
+        setPageData(pd );
+      }).catch( (err) => {
+        console.log( err);
+
+      });
+  
+    }, []
   );
+  
+  
+  return (
+    <pre id="content" className="App" dangerouslySetInnerHTML={{__html:pageData.content}}/>
+    );
 }
 
 export default App;
