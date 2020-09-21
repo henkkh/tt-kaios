5414e0f85e1e5209131b6393d445672fac3fa509
Henk Klijn Hesselink <klijnh@oclc.org>
2020-09-21 12:49:54 +0200

    first commit
    chore fit in screen
    
    added routing and key support added
    
    removed router
    
    abc
    
    working with request params
    
    moved ttf
    
    state page
    
    Eindelijk werkend

--- README.md
+++ README.md
@@ -1,44 +1,45 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<style>
+r { color: Red; background: Black }
+y { color: Yellow; background: Black }
+g { color: #0F0; background: Black }
+c { color: Cyan; background: Black }
+</style>
 
-## Available Scripts
+# NOS Teletekst
 
-In the project directory, you can run:
+**Sinds 1 april 1980 actief. Nu ook naar Kaios na 45 jaar!**
 
-### `npm start`
+Razendsnel toegang tot NOS Teletekst met onder andere nieuws, verkeersinformatie, weer, sportnieuws en natuurlijk sportuitslagen.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+![start](./img/100.png)
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+## Navigatie
 
-### `npm test`
+Navigatie kan op verschillende manieren:
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+* Met de *Softkeys* LSK en RSK navigeer je naar de vorige resp. de volgende (sub)pagina.
+* Met de nummers op het *Number pad* spring je naar een specifieke pagina na het invoeren van 3 nummers.
+* Indien pagina niet gevonden is kan met CSK terug naar pagina 100 gesprongen worden
+* Met 1x # gevolgd door CSK spring naar het <r>rode</r> item
+* Met 2x # gevolgd door CSK spring naar het <g>groene</g> item
+* Met 3x # gevolgd door CSK spring naar het <y>gele</y> item
+* Met 4x # gevolgd door CSK spring naar het <c>cyane</c> item
 
-### `npm run build`
+![keypad](./img/keypad.png)
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+## Hoe te verkrijgen
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Het is de bedoeling de app op de Kaios Store ter beschikking te maken. Daarnaast kan het van github gedownload worden:
 
-### `npm run eject`
+````bash
+git clone https://github.com/henkkh/tt-kaios
+````
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+## Zelf installeren
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+Gebruik de Web IDE in  [Waterfox Classic](https://classic.waterfox.net/) om de app op je device te laden. Bijvoorbeeld de Nokia Flip 2720
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+![801](./img/801.png)
---
+++ js/app.js
@@ -0,0 +1,147 @@
+// DOMContentLoaded is fired once the document has been loaded and parsed,
+// but without waiting for other external resources to load (css/images/etc)
+// That makes the app more responsive and perceived as faster.
+// https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
+window.addEventListener('DOMContentLoaded', function () {
+
+  // We'll ask the browser to use strict code to help us catch errors earlier.
+  // https://developer.mozilla.org/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode
+  'use strict';
+
+  var APIURL = "https://teletekst-data.nos.nl/json?p=";
+
+  var notFoundHtml = `<pre id="content" tabindex="10">                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+  Pagina niet gevonden...
+  Ga terug naar de <a class="cyan" href="/?p=100">startpagina</a>
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+</pre>
+`;
+
+  var enteredPage = 0;
+  var nextPage;
+  var prevPage;
+  var fastTextLinks;
+  var ftlIdx = 0;
+
+
+  document.onkeydown = function (ev) {
+    var keyCode = ev.keyCode || ev.which;
+    // console.log(fastTextLinks[ftlIdx].page);
+    // var key = {
+    //   left: 37,
+    //   up: 38,
+    //   right: 39,
+    //   down: 40,
+    //   pageDown: 34,
+    //   pageUp: 33
+    // };
+    const num = (keyCode - 48);
+    if (num >= 0 && num <= 9) {
+      enteredPage = enteredPage * 10 + num;
+      if (enteredPage > 99) {
+        fetchPage("" + enteredPage);
+        enteredPage = 0;
+      }
+      return;
+    }
+    switch (ev.key) {
+      case 'SoftLeft':
+        fetchPage(prevPage);
+        break;
+      case 'SoftLeft':
+        fetchPage(prevPage);
+        break;
+
+      case 'SoftRight':
+        fetchPage(nextPage);
+      break;
+
+      case 'Enter':
+          // Action case press center key
+          if ( enteredPage === 100 ) {
+            fetchPage("" + enteredPage);
+            enteredPage = 0;
+          }
+          break;
+      case '#':
+        enteredPage = fastTextLinks[ftlIdx].page;
+        ftlIdx = ++ftlIdx % fastTextLinks.length;
+        return;
+      }
+
+    enteredPage = 0;
+    return;
+  }
+
+
+  var request = null;
+
+  function fetchPage(page) {
+
+    var url = APIURL + page;
+
+    // If you don't set the mozSystem option, you'll get CORS errors (Cross Origin Resource Sharing)
+    // You can read more about CORS here: https://developer.mozilla.org/docs/HTTP/Access_control_CORS
+    request = new XMLHttpRequest({ mozSystem: true });
+
+    request.open('get', url, true);
+    request.responseType = 'json';
+    request.addEventListener('error', onRequestError);
+    request.addEventListener('load', onRequestLoad);
+
+    request.send();
+
+  }
+
+
+  function onRequestError() {
+    enteredPage = 100;
+    document.getElementById("content").innerHTML = notFoundHtml;
+  }
+
+
+  function onRequestLoad() {
+    if (request.status == 404) {
+      onRequestError();
+      return;
+    }
+    const resp = request.response;
+    nextPage = resp.nextSubPage || resp.nextPage;
+    prevPage = resp.prevSubPage || resp.prevPage;
+    fastTextLinks = resp.fastTextLinks
+    ftlIdx = 0;
+
+    var tto = request.response.content;
+    tto = tto.replace(/href=\"#/g, "href=\"/?p=");
+
+    document.getElementById("content").innerHTML = tto;
+  }
+
+  var url = new URL(window.location.href);
+  fetchPage(url.searchParams.get("p") || "100");
+
+  // Enter full-screen mode
+  document.documentElement.requestFullscreen();
+
+});
