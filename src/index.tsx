64b69bdec7eb0271c2bae4bf1f6428b4554e25b2
Henk W. Klijn Hesselink <henkkh@live.com>
2025-03-06 14:41:12 +0100

    abc

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Teletekst from './components/Teletekst';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Softkey } from './components/Softkey/Softkey';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Teletekst id='100'/>
-    <Softkey left="nieuws" onKeyLeft='s' onKeyCenter='s' onKeyRight='s' center="100" right="sport" ></Softkey>
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
-
+root.render(
+  <React.StrictMode>
+    <Teletekst id="100"/>
+    <Softkey left="nieuws" onKeyLeft='s' onKeyCenter='s' onKeyRight='s' center="100" right="sport" ></Softkey>
+  </React.StrictMode>
+);
